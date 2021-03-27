9f9c867c1d8b9b681ff545f0831ebc276482ac1b
law911012eqw <beaverbryanantipolo@gmail.com>
2021-03-27 15:51:00 -0400

    Implemented initial functionality of battle component.

--- src/Components/Battle.jsx
+++ src/Components/Battle.jsx
@@ -1,63 +1,103 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import ReturnToMenu from './ReturnToMenu';
-import main from '../scripts/main'
+import { P1, P2 } from '../scripts/main'
 
 export default function Battle() {
     const [width] = useState(window.innerWidth);
-    const [height] = useState(window.innerHeight);
-    const [boardWidth] = useState(10);
-    const [boardHeight] = useState(10);
+    const BOARDWIDTH = P1.gameboard.width;
+    const BOARDHEIGHT = P1.gameboard.height;
     const cv1Ref = useRef(null);
     const cv2Ref = useRef(null);
+    const SIZE = width * .20;
+    console.log(BOARDHEIGHT, BOARDWIDTH);
+    //draw tiles for the board
+    const drawSquare = (x, y, ctx, sz) => {
+        ctx.fillStyle = 'blue';
+        ctx.strokeStyle = 'rgba(34,34,34,0.3)';
+        ctx.strokeRect(x * sz, y * sz, sz, sz);
+    }
 
-    const drawStrokeRect = (bw, bh, w, ctx1, ctx2) => {
-        const size = width * .20;
-        const tileSize = 0.1 * size;
-        ctx1.strokeStyle = 'black';
-        ctx2.strokeStyle = 'black';
-        for (let i = 0; i < bw; i++) {
-            ctx1.strokeRect((i+1*bw + size) - tileSize, i+1*bw + size, w / 10, w / 10);
-            ctx2.strokeRect((i+1*bw + size) - tileSize, i+1*bw + size, w / 10, w / 10);
-            for (let j = 0; j < bh; j++) {
-                ctx1.strokeRect(w / 10, w / 10, w / 10, w / 10);
-                ctx2.strokeRect(w / 10, w / 10, w / 10, w / 10);
-            }
-        }
+    //draw the game board
+    const drawBoard = (ctx, sz, board) => {
+        const SQR = 0.1 * sz;
+        ctx.clearRect(width,width,width,width)
+        board.forEach((row, r) => {
+            row.forEach((col, c) => {
+                if(col[1] === 1){
+                    drawX(c, r, ctx, SQR);
+                }
+                drawSquare(c, r, ctx, SQR);
+            })
+        })
+    }
+
+    const drawX = (x, y, ctx, sz) => {
+        ctx.beginPath();
+
+        ctx.moveTo((x * sz) - 20, (y * sz) - 20);
+        ctx.lineTo((x * sz) + 20, (y * sz) + 20);
+
+        ctx.moveTo((x * sz) + 20, (y * sz) - 20);
+        ctx.lineTo((x * sz) - 20, (y * sz) + 20);
+        ctx.stroke();
+    }
+    const canvasContainer = (ref, size, id) => {
+        return (
+            <div>
+                <canvas
+                    ref={ref}
+                    className="cv"
+                    id={id}
+                    width={`${size}px`}
+                    height={`${size}px`}
+                >
+
+                </canvas>
+            </div>
+        )
+    }
+    const shipContainer = () => {
+
     }
     useEffect(() => {
         const cv1 = cv1Ref.current;
         const cv2 = cv2Ref.current;
         const ctx1 = cv1.getContext('2d');
         const ctx2 = cv2.getContext('2d');
-        drawStrokeRect(boardWidth, boardHeight, width, ctx1, ctx2);
+        drawBoard(ctx1, SIZE, P1.gameboard.board);
+        drawBoard(ctx2, SIZE, P2.gameboard.board);
     }, [])
     return (
         <div id="Battle">
             <ReturnToMenu />
             <h1>Battle</h1>
             <div id="main-battle">
-                <div>
+                {canvasContainer(cv1Ref, SIZE, "cv1")}
+                {canvasContainer(cv2Ref, SIZE, "cv2")}
+                {/* <div>
                     <canvas
                         ref={cv1Ref}
                         className="cv"
                         id="cv1"
-                        width={`${width * 0.20}px`}
-                        height={`${width * 0.20}px`}
+                        width={`${SIZE}px`}
+                        height={`${SIZE}px`}
                     >
 
                     </canvas>
+                    <div></div>
                 </div>
                 <div>
                     <canvas
                         ref={cv2Ref}
                         className="cv"
                         id="cv2"
-                        width={`${width * 0.20}px`}
-                        height={`${width * 0.20}px`}
+                        width={`${SIZE}px`}
+                        height={`${SIZE}px`}
                     >
                     </canvas>
-                </div>
+                    <div></div>
+                </div> */}
             </div>
         </div>
     )
